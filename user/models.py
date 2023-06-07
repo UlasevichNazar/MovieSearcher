fe5310db271740aafa5d8949c208db3c0faa3e10
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-06-07 18:48:24 +0300

    feat: start_project

---
+++ user/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth import get_user_model
+#
+#
+# class User(AbstractUser):
+#     username = models.CharField('Nickname', max_length=255, unique=True)
+#     email = models.EmailField('Email', unique=True)
+#     mailing_list_for_users = models.BooleanField('Mailing_list', )
+
+
