2bcc3f0ffe31a3fed0b063abbb74c4e3f0e0856f
dima <DmitriBe1ov>
2023-06-10 19:13:59 +0300

    userprofile

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from user.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
-    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="profile/")
     facebook = models.CharField(max_length=50, null=True, blank=True)
     twitter = models.CharField(max_length=50, null=True, blank=True)
     instagram = models.CharField(max_length=50, null=True, blank=True)
