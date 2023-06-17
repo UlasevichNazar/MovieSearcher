8a79463a9f183f2f2a57b1f7a4e5277c3ee1cb5a
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-06-17 03:56:58 +0300

    feat: soon_movies

---
+++ movies/templatetags/movie_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+from movies.models import Movie
+
+register = template.Library()
+
+
+@register.inclusion_tag('movies/soon_on_site/movies.html')
+def get_movie(count):
+    movies = Movie.objects.filter(status="DF")[:count]
+    return {'soon_movies': movies}
