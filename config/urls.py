bb376b6cdb1825922772ae8b226802fdb7bd5674
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-06-24 14:01:41 +0300

    fixed: config-urls

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('movies.urls')),
-    path('user/', include('userprofile.urls')),
-    
-
     path("admin/", admin.site.urls),
     path("", include("movies.urls")),
     path("user/", include("userprofile.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
-    path('reg/', include('reg_login.urls')),
+    path("reg/", include("reg_login.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
